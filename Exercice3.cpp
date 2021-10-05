720ec9eb4497fe24db91e2fa6ae6b0c842ae6cbd
DECHIRM <90182127+DECHIRM@users.noreply.github.com>
2021-10-05 08:29:07 +0200

    Add files via upload

--- Exercice3.cpp
+++ Exercice3.cpp
@@ -3,12 +3,11 @@
 #include <stdlib.h>
 #include <conio.h>
 
-int main() {
-
+int main() 
+{
 	float t[3][4];
-	float sRow, sCol, somme = 0;
+	float ligne, colonne, somme = 0;
 	int i, j = 0;
-
 	printf(" Entrez les %d éléments du tableau : ", 3 * 4);
 
 	for (i = 0; i < 3; i++) {
@@ -16,22 +15,18 @@ int main() {
 			scanf("%f", &t[i][j]);
 		}
 	}
-
 	for (i = 0; i < 3; i++) {
-		sRow = 0;
+		ligne = 0;
 		for (j = 0; j < 4; j++) {
-			sRow = sRow + t[i][j];
+			ligne = ligne + t[i][j];
 		}
 	}
-
 	for (j = 0; j < 4; j++) {
-		sCol = 0;
+		colonne = 0;
 		for (i = 0; i < 3; i++) {
-			sCol = sCol + t[i][j];
+			colonne = colonne + t[i][j];
 		}
 	}
-
-	somme = sRow + sCol;
-	printf(" La somme des éléments de t est de : %f", somme);
-
+	somme = ligne + colonne;
+	printf(" La somme des éléments : %f", somme);
 }
